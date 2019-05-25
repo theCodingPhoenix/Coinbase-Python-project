7f9cbd21233d6a5eb32ef665445903d909718d9b
aakanksha-bhandari <aakanksha.bhandari@gmail.com>
2019-05-25 17:36:29 +0100

    Converted the feed to a class and added another test

--- test_coinbase_ws_feed.py
+++ test_coinbase_ws_feed.py
@@ -8,4 +8,12 @@ def test_coinbase_ws_feed_URL():
     assert coinbase_ws_feed.URL == "wss://ws-feed.pro.coinbase.com"
 
 
+def test_coinbase_ws_feed_is_file_created():
+    # this establishes the connection and checks the on_message method to ensure
+    # a file is created by the name trades.csv
+    ws = coinbase_ws_feed.CoinbaseFeed()
+    message = '{"type":"ticker","product_id":"BTC-USD","price":"8043.15000000","time":"2019-05-25T16:19:31.290000Z","last_size":"0.01784403"}'
+    ws.on_ws_message(message)
+    trades_file = Path("trades.csv")
+    assert trades_file.is_file() == True
 
