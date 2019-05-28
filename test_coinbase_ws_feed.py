6e1e1764a1be94c72a4434c130d2eca4e7ac6214
aakanksha-bhandari <aakanksha.bhandari@gmail.com>
2019-05-28 10:42:25 +0100

    Refactored code and added some more tests

--- test_coinbase_ws_feed.py
+++ test_coinbase_ws_feed.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 import coinbase_ws_feed
 
 
@@ -8,12 +9,29 @@ def test_coinbase_ws_feed_URL():
     assert coinbase_ws_feed.URL == "wss://ws-feed.pro.coinbase.com"
 
 
+def get_example_trade():
+    return '{"type":"ticker","product_id":"BTC-USD","price":"8043.15000000","time":"2019-05-25T16:19:31.290000Z","last_size":"0.01784403"}'
+
+
+# accepts the file path and returns True/False if the file is empty
+def is_file_empty(file_path):
+    return os.stat(file_path) == 0
+
 def test_coinbase_ws_feed_is_file_created():
     # this establishes the connection and checks the on_message method to ensure
     # a file is created by the name trades.csv
     ws = coinbase_ws_feed.CoinbaseFeed()
-    message = '{"type":"ticker","product_id":"BTC-USD","price":"8043.15000000","time":"2019-05-25T16:19:31.290000Z","last_size":"0.01784403"}'
-    ws.on_ws_message(message)
+    ws.on_ws_message(get_example_trade())
     trades_file = Path("trades.csv")
+    # the file should be created
     assert trades_file.is_file() == True
+    # the file should not be empty
+    assert is_file_empty('trades.csv') == False
+
+
+# test that we are printing exactly what's required to the file
+# i.e. we only require to print the Time and Price field,
+def test_get_line_for_file():
+    line_for_file = "Time: 2019-05-25T16:19:31.290000Z, Price: 8043.15000000"
+    assert line_for_file == coinbase_ws_feed.get_line_for_file(get_example_trade())
 
